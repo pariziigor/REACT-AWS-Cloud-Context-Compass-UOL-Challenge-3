6dba92bd61b33f49c2e1046ab36cede575f65482
Igor Parizi <100965139+pariziigor@users.noreply.github.com>
2023-08-24 16:26:53 -0300

    🚧 Fixed some bugs in profile component

--- src/app/components/ProfileComponent/ProfileComponent.tsx
+++ src/app/components/ProfileComponent/ProfileComponent.tsx
@@ -1,13 +1,16 @@
 import { AsideComponents } from "../AsideInfo/Index";
 import { CardComponents } from "../CardImage/Index";
-import ProfileImage from "../CardImage/ProfileImage";
 import { InfoComponents } from "../Profile/Index";
 
 function ProfileComponent() {
   return (
     <div className="gap-8 justify-center items-center  w-full flex flex-col py-10 lg:flex-row">
       <CardComponents.Root>
-        <CardComponents.Img src="https://i.imgur.com/jguCgC7.png" textName="Hello, world!" textInfo="Solteiro, Brasil" />
+        <CardComponents.Img
+          src="https://i.imgur.com/jguCgC7.png"
+          textName="Hello, world!"
+          textInfo="Solteiro, Brasil"
+        />
         <CardComponents.Button textButton="Editar meu perfil" />
       </CardComponents.Root>
       <InfoComponents.Root>
@@ -16,107 +19,139 @@ function ProfileComponent() {
         <InfoComponents.Emoji />
         <InfoComponents.Tags />
       </InfoComponents.Root>
-
       <AsideComponents.Root>
         <AsideComponents.Container>
-          <AsideComponents.Link href="/profile" text="Amigos (248)" className="text-base text-dark-10" />
-          <AsideComponents.Link href="/profile" text="Ver todos" className="text-sm text-brand-color" />
+          <AsideComponents.Link
+            href="/profile"
+            text="Amigos (248)"
+            className="text-base text-dark-10"
+          />
+          <AsideComponents.Link
+            href="/profile"
+            text="Ver todos"
+            className="text-sm text-brand-color"
+          />
         </AsideComponents.Container>
         <AsideComponents.ContainerBody>
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/AFJ9Uhs.png" />
+            src="https://i.imgur.com/AFJ9Uhs.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/g00WRng.png" />
+            src="https://i.imgur.com/g00WRng.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/zGWdEfN.png" />
+            src="https://i.imgur.com/zGWdEfN.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/NCNMWMq.png" />
+            src="https://i.imgur.com/NCNMWMq.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/rZ8fCOP.png" />
+            src="https://i.imgur.com/rZ8fCOP.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/OZzAEqY.png" />
+            src="https://i.imgur.com/OZzAEqY.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Nanda"
-            src="https://i.imgur.com/iXETVnZ.png" />
+            src="https://i.imgur.com/iXETVnZ.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fernando"
-            src="https://i.imgur.com/NrsHF8b.png" />
+            src="https://i.imgur.com/NrsHF8b.png"
+          />
           <AsideComponents.Object
             href="/profile"
             round
             text="Fer"
-            src="https://i.imgur.com/smEE1fr.png" />
+            src="https://i.imgur.com/smEE1fr.png"
+          />
         </AsideComponents.ContainerBody>
       </AsideComponents.Root>
       <AsideComponents.Root>
         <AsideComponents.Container>
-          <AsideComponents.Link href="/profile" text="Comunidade (42)" className="text-base text-dark-10" />
-          <AsideComponents.Link href="/profile" text="Ver todos" className="text-sm text-brand-color" />
+          <AsideComponents.Link
+            href="/profile"
+            text="Comunidade (42)"
+            className="text-base text-dark-10"
+          />
+          <AsideComponents.Link
+            href="/profile"
+            text="Ver todos"
+            className="text-sm text-brand-color"
+          />
         </AsideComponents.Container>
         <AsideComponents.ContainerBody>
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/7N7C9nu.png" />
+            src="https://i.imgur.com/7N7C9nu.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/tBIGWCc.png" />
+            src="https://i.imgur.com/tBIGWCc.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/gwnknXd.png" />
+            src="https://i.imgur.com/gwnknXd.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/DCN6mIZ.png" />
+            src="https://i.imgur.com/DCN6mIZ.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/qCvbyYR.png" />
+            src="https://i.imgur.com/qCvbyYR.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/nrIosPU.png" />
+            src="https://i.imgur.com/nrIosPU.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Nanda"
-            src="https://i.imgur.com/GejNWt4.png" />
+            src="https://i.imgur.com/GejNWt4.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fernando"
-            src="https://i.imgur.com/ROqwqw4.png" />
+            src="https://i.imgur.com/ROqwqw4.png"
+          />
           <AsideComponents.Object
             href="/profile"
             text="Fer"
-            src="https://i.imgur.com/bX3Ifjp.png" />
+            src="https://i.imgur.com/bX3Ifjp.png"
+          />
         </AsideComponents.ContainerBody>
       </AsideComponents.Root>
-
     </div>
-  )
+  );
 }
 
 export default ProfileComponent;
